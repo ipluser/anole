562a5f6858d1d58be5f1c6c755ecbfe8c0a1581e
ipluser <ipluser@163.com>
2016-10-07 16:28:06 +0800

    feat(utils): add isArray function

--- test/utils.test.js
+++ test/utils.test.js
@@ -11,10 +11,28 @@ describe('Utils', () => {
     it('should be false', () => {
       (Utils.isFunction()).should.be.false();
       (Utils.isFunction(null)).should.be.false();
-      (Utils.isFunction(100)).should.be.false();
-      (Utils.isFunction('100')).should.be.false();
+      (Utils.isFunction(undefined)).should.be.false();
+      (Utils.isFunction(0)).should.be.false();
+      (Utils.isFunction('0')).should.be.false();
       (Utils.isFunction({})).should.be.false();
       (Utils.isFunction([])).should.be.false();
+    });
+  });
+
+  describe('#isArray', () => {
+    it('should be true', () => {
+      (Utils.isArray([])).should.be.true();
+      (Utils.isArray(Array(1, 2))).should.be.true();  // eslint-disable-line no-array-constructor
+    });
+
+    it('should be false', () => {
+      (Utils.isArray()).should.be.false();
+      (Utils.isArray(null)).should.be.false();
+      (Utils.isArray(null)).should.be.false();
+      (Utils.isArray(0)).should.be.false();
+      (Utils.isArray('0')).should.be.false();
+      (Utils.isArray({})).should.be.false();
+      (Utils.isArray(() => {})).should.be.false();
     });
   });
 });
